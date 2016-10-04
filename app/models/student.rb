da86222021e9446a3874446cc397cfbc8607c926
osoyei <yeison-arley25@hotmail.com>
2016-10-04 15:55:59 +0000

    rutas, validaciones y api students, solo probado get

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ApplicationRecord
+  self.primary_key = 'id'
+  validates :studentCode, presence: true, uniqueness: true, numericality: true
+  validates :email, presence: true, uniqueness: true, format: {with: /(.*)@eafit.edu.co/, message: "sólo correo universitario"}
+  validates :name, presence: true
+  validates :phone, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Api definition
+  namespace :api, defaults: { format: :json} do #, path: '/', defaults: { format: :json} do
+    
+     #scope module: :v1 do
+       # We are going to list our resources here
+       resources :students #, only: [:index, :create, :destroy, :update, :show]
+     #end
+  end 
 end
