3190fbc075865c6a3b9aa0e162cb02f9d1419f13
osoyei <yeison-arley25@hotmail.com>
2016-10-11 16:04:19 +0000

    elementos con validaciones

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,2 +1,6 @@
 class Element < ApplicationRecord
+    self.primary_key = 'id'
+    validates :id, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :description, presence: true
 end
