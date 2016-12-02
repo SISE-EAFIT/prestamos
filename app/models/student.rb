15f2838354840b8a487fe7dcac091e6f783c3d24
osoyei <yeison-arley25@hotmail.com>
2016-12-02 17:49:28 +0000

    comentadas validaciones

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,8 +3,8 @@ class Student < ApplicationRecord
   has_many :lends, dependent: :destroy
   self.primary_key = 'id'
   validates :id, presence: true, uniqueness: true, numericality: true
-  validates :studentCode, presence: true, uniqueness: true, numericality: true
-  validates :email, presence: true, uniqueness: true, format: {with: /(.*)@eafit.edu.co/, message: "sólo correo universitario"}
-  validates :name, presence: true
-  validates :phone, presence: true
+  # validates :studentCode, presence: true, uniqueness: true, numericality: true
+  # validates :email, presence: true, uniqueness: true, format: {with: /(.*)@eafit.edu.co/, message: "sólo correo universitario"}
+  # validates :name, presence: true
+  # validates :phone, presence: true
 end
